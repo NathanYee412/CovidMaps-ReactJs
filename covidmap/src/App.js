c6698aeabd6f85107e5e39218961a60c1c51104a
Nathan Yee <nyee@csu.fullerton.edu>
2020-12-01 20:55:05 -0800

    first commit

---
+++ covidmap/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Covid Maps
+      </h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <iframe src="https://public.domo.com/cards/dG1jy" width="100%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+          </div>
+          <div class="col-lg-6">
+          <iframe src="https://public.domo.com/cards/axpDJ" width="100%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
